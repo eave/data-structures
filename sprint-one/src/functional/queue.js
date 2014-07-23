963e271332e6fb70c533245d77639590a01e975d
Mario <mpontice@gmail.com>
2014-07-23 12:23:14 -0700

    solve functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    if (size === 0) {
+      storage[size] = value;
+      size++;
+    } else {
+      for (var i = size - 1; i > -1; i--) {
+        storage[i+1] = storage[i];
+      }
+      storage[0] = value;
+      size++; 
+    };
   };
 
   someInstance.dequeue = function(){
+    size && size--; //size will pass if > 0, and will then evaluate the second half of the statement
+    var result = storage[size];
+    delete storage[size];
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
