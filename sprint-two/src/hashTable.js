0994787bccc3a847d253a4ffec9382e70b2ecc2c
eave <mpontice@gmail.com>
2014-07-24 21:37:36 -0700

    Can has solve all hashTable prompts? yes

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,12 +1,3 @@
-// a hash table (also hash map) is a data structure used to implement
-// an associative array, a structure that can map keys to values.
-
-// A hash table uses a hash function to compute an index into an array
-// of buckets or slots, from which the correct value can be found.
-
-// PS: JavaScript Objects are essentially all hash tables (though not necessarily literally)
-
-
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
@@ -14,18 +5,28 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  if (this._storage[i] === undefined) {
+    this._storage[i] = {};
+    this._storage[i][k] = v;
+  }
+  this._storage[i][k] = v;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  if (this._storage[i] === undefined) {
+    return null;
+  } else {
+    return this._storage[i][k];
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i][k] = null;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
